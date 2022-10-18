a58815b0cb811dc37f66e583a1bd6b760ec90778
BABJI-AWS-DEVOPS <babji.drru@gmail.com>
2022-10-18 16:29:28 +0530

    First Commit

---
+++ src/main/java/com/babjiawsdevops/App.java
@@ -0,0 +1,13 @@
+package com.babjiawsdevops;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
